9c574335fa52600549d8a4cd451e9994a5f9ea1a
Marc Schwarze <marc.schwarze@posteo.de>
2020-04-20 13:52:25 +0200

    Add new route in app.py.

--- 08_Templates/app.py
+++ 08_Templates/app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    user_logged_in = True
+    return render_template('home.html', user_logged_in=user_logged_in)
+
+
+@app.route('/puppy/<name>')
+def puppy(name):
+    return render_template('puppy.html', name=name)
 
 
 if __name__ == "__main__":
