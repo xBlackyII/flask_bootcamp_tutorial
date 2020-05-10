3b1f901b615e42134bd3e78924732b775c752cc9
Marc Schwarze <marc.schwarze@posteo.de>
2020-05-10 16:36:36 +0200

    Start implementing blog.

--- 13_SocialCompanyBlog/puppycompanyblog/__init__.py
+++ 13_SocialCompanyBlog/puppycompanyblog/__init__.py
@@ -1,3 +1,30 @@
+from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
+from puppycompanyblog.users.views import users
 from flask import Flask
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import os
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# DB Setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+##
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
+app.register_blueprint(users)
